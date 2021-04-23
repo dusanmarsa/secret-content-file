0ac1f69f25a4624837facd8c2d5c0c3e95e464b5
Dušan Marsa <marsa@aliup.cz>
2021-04-23 10:17:57 +0200

    test

--- src/action.js
+++ src/action.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 const path = require('path');
 const core = require('@actions/core');
 
+const fsPromises = fs.promises
+
 async function run() {
     try {
         const fileContent = core.getInput('CONTENT');
@@ -10,18 +12,18 @@ async function run() {
         const fileAbsolutePath = path.join(process.cwd(), filePath)
 
         try {
-            await fs.access(fileAbsolutePath)
+            await fsPromises.access(fileAbsolutePath)
         } catch (error) {
-            await fs.mkdir(fileAbsolutePath, { recursive: true })
+            await fsPromises.mkdir(fileAbsolutePath, { recursive: true })
         }
 
         try {
-            await fs.access(fileAbsolutePath)
+            await fsPromises.access(fileAbsolutePath)
         } catch (error) {
             core.setFailed("couldn't create directory structure");
         }
 
-        await fs.writeFile(path.join(fileAbsolutePath, fileName), fileContent)
+        await fsPromises.writeFile(path.join(fileAbsolutePath, fileName), fileContent)
     } catch (error) {
         core.setFailed(error.message);
     }
